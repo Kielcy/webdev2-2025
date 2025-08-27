381642c58d3faecc799bd18103292c6d25366735
johnverz22 <johnny.verzola@lorma.edu>
2025-08-27 10:23:48 +0800

    Initial Commit

---
+++ src/main/java/com/johnverz/webdev1_g1/Car.java
@@ -0,0 +1,49 @@
+package com.johnverz.webdev1_g1;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String make;
+    private String model;
+    private int year;
+    private String color;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
